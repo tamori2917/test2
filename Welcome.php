0570787e590be28f918ab18b47b5423242a39529
Mori <fj5608ed@aa.jp.fujitsu.com>
2018-02-20 12:57:36 +0900

    0220 commit

--- Welcome.php
+++ Welcome.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>Hello World</title>
-</head>
-<body>
-<p><?php print("ends-with wlecome.com") ?></p>
-</body>
-</html>
+ends-with wlecome.com
+equals welwelcome.com
+contains welwelwelcome.com
